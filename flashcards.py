6ac80bb5c2b0e54933bd3c4f8995ef2be76f7135
Zabir Hasan <dominicancongo@gmail.com>
2024-10-20 21:09:00 +0600

    a

---
+++ flashcards.py
@@ -0,0 +1,18 @@
+class flashcard:
+    def __init__(self,word,meaning):
+        self.word = word
+        self.meaning = meaning
+    def __str__(self):
+        return f"{self.word} ({self.meaning})"
+flash = []
+print("Welcome to flashcard maker")
+while(True):
+    word = input("Enter  the question you want in the flashcard : ")
+    meaning = input("Enter the answer : ")
+    flash.append( flashcard(word,meaning) )
+    option = int(input("Enter 0 if you want to add another flashcard, otherwise enter 1 : "))
+    if(option):
+        break
+print("\nYour Flashcard is ready")
+for i in flash:
+    print(">",i  )
